6248c57f74c27246e6dad4a41b19cfc353b8096f
James Wang <jamesnewking@gmail.com>
2018-06-14 21:53:24 -0700

    react-intro | James Wang

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+let luckyNumber = function(){
+    const randomNum = Math.floor(Math.random()*1001);
+    return randomNum;
+};
+
+let userObj = {
+    name: 'Cooper Dog',
+    luckyNumber: luckyNumber
+};
+
+let greeting = function(name, luckynumber){
+    return <div className="container">
+        <h1>Howdy {name}!</h1>
+        <h2 className="text-muted">Your lucky number is {luckynumber}!</h2>
+    </div>
+};
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(userObj.name,userObj.luckyNumber()),
     document.getElementById('root')
 );
